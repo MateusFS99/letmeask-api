6d848d443fc8880c189a7496d09085972df66736
Mateus Fonseca Santos <mateusfsantos.pfs@gmail.com>
2025-07-12 22:50:51 -0300

    feat: add audio chunks table and implement audio upload route with transcription and embedding generation

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastify } from "fastify";
+import { fastifyMultipart } from "@fastify/multipart";
 import {
   serializerCompiler,
   validatorCompiler,
@@ -11,12 +12,14 @@ import { getRoomsRoute } from "./http/routes/get-rooms.ts";
 import { createRoomRoute } from "./http/routes/create.room.ts";
 import { getRoomQuestions } from "./http/routes/get-room-questions.ts";
 import { createQuestionRoute } from "./http/routes/create-question.ts";
+import { uploadAudioRoute } from "./http/routes/upload-audio.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
   origin: "http://localhost:5173",
 });
+app.register(fastifyMultipart);
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -29,6 +32,7 @@ app.register(getRoomsRoute);
 app.register(createRoomRoute);
 app.register(getRoomQuestions);
 app.register(createQuestionRoute);
+app.register(uploadAudioRoute);
 
 app.listen({ port: env.PORT }).then(() => {
   console.log(`Server Running on Port ${env.PORT}`);
